8bb43ef42c09d182a8fc53c9e92fdd70113f3147
Neven <58472164+neven14@users.noreply.github.com>
2021-10-18 09:21:27 +0200

    Add files via upload

---
+++ zad2_Zrakoplov/zad2_Zrakoplov/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad2_Zrakoplov
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            Zrakoplov x380 = new Zrakoplov("Airbus", 2000, 6000);
+            Zrakoplov cesnna162 = new Zrakoplov("Cessna", 75, 870);
+            Zrakoplov pc21 = new Zrakoplov("Pilatus", 1200, 1333);
+
+        }
+    }
+}
---
+++ zad2_Zrakoplov/zad2_Zrakoplov/Zrakoplov.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad2_Zrakoplov
+{
+    public class Zrakoplov
+    {
+        private string Naziv;
+        private double SnagaMotora;
+        private int DosegLeta;
+
+        public Zrakoplov(string naziv, double snagaMotora, int dosegLeta)
+        {
+            Naziv = naziv;
+            SnagaMotora = snagaMotora;
+            DosegLeta = dosegLeta;
+        }
+    }
+}
